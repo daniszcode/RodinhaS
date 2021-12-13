a38088ff8d8ebbb8118430aaae35721bb372f2f5
Daniele Marques <danirbd13@hotmail.com>
2021-12-13 20:23:23 -0300

    Criação das tags HTML + components da Page Map

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -1,20 +1,29 @@
 import { MapPosition } from "../../Components/MapPosition/MapPosition";
 import { ArrowIcon } from "../../Components/ArrowIcon/ArrowIcon"
+import { Icon } from "../../Components/Icon/icon";
 const Map = () => {
     return (
         <>   
         <body>
             <main>
                 <header>
-                <div>
                     <div>
                         <ArrowIcon />
                     </div>
+                    <div>
+                        <Icon />
+                        <h1>Como Chegar</h1>
                 </div>
                 </header>
+                <section>
+                <MapPosition  />
+                </section>
+                <footer>
+                    <Icon />
+                    <p>Indica sua localização</p>
+                </footer>
             </main>
         </body>
-        <MapPosition  />
         </>
     )
 }
