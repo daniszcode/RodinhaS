4e4633fdcc0e64f8f55003122966cc80f6e8243a
Daniele Marques <danirbd13@hotmail.com>
2021-12-14 11:20:30 -0300

    Crando estilização do header e da section da Page Map

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -1,18 +1,19 @@
 import { MapPosition } from "../../Components/MapPosition/MapPosition";
 import { ArrowIcon } from "../../Components/ArrowIcon/ArrowIcon"
 import { Icon } from "../../Components/Icon/icon";
+import "./Map.css"
 const Map = () => {
     return (
         <>   
-        <body>
+        <body className="container-map">
             <main className="container-main--map">
                 <header className="container-header--map">
                     <div className="box-header--map">
                         <ArrowIcon />
                     </div>
                     <div className="box-header">
-                        <Icon />
-                        <h1 className="box-header--text">Com<span className="box-header--span">o</span> Chega<span>r</span></h1>
+                        <Icon imageGirlInwheelchair="box-header--iconGirl" />
+                        <h1 className="box-header--text">Com<span className="box-header--span">o</span> Chega<span className="box-header--span">r</span></h1>
                     </div>
                 </header>
                 <section className="container-section--map">
