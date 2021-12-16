5186875e5029374e0fd5bf5570bc973aa96eb2fb
Daniele Marques <danirbd13@hotmail.com>
2021-12-16 13:07:11 -0300

    Ajeitando bug do deploy

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,12 @@ import { Login } from "./Pages/Login/Login";
 import { Home } from "./Pages/Home/Home";
 import { Ranking } from "./Pages/Ranking/Ranking";
 import { Map } from "./Pages/Map/Map";
+
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Login} />
         <Route path="/home" component={Home} />
         <Route path="/ranking" component={Ranking} />
         <Route path="/map" component={Map} />
